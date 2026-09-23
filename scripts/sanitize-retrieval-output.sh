d79067ada24ab3ceae6f4e61084c80e46f5b9e7e
chasekb <106553078+chasekb@users.noreply.github.com>
2026-09-23 14:07:05 -0500

    fix: support quoted and unquoted credential values

--- scripts/sanitize-retrieval-output.sh
+++ scripts/sanitize-retrieval-output.sh
@@ -2,6 +2,6 @@
 set -Eeuo pipefail
 
 sed -E \
-  -e "s/(COINBASE_API_KEY|COINBASE_API_SECRET|COINBASE_API_PASSPHRASE|POSTGRES_DB_PASSWORD|DB_PASSWORD)=[\"'][^[:space:]\"']+[\"']?/\\1=[REDACTED]/Ig" \
+  -e "s/(COINBASE_API_KEY|COINBASE_API_SECRET|COINBASE_API_PASSPHRASE|POSTGRES_DB_PASSWORD|DB_PASSWORD)=[\"']?[^[:space:]\"']+[\"']?/\\1=[REDACTED]/Ig" \
   -e "s/([\"']?[Aa][Uu][Tt][Hh][Oo][Rr][Ii][Zz][Aa][Tt][Ii][Oo][Nn][\"']?[[:space:]]*:[[:space:]]*[\"']?)([Bb][Ee][Aa][Rr][Ee][Rr]|[Bb][Aa][Ss][Ii][Cc])[[:space:]]+[^\"']+[\"']?/\\1\\2 [REDACTED]/g" \
   -e "s/([Aa][Uu][Tt][Hh][Oo][Rr][Ii][Zz][Aa][Tt][Ii][Oo][Nn][[:space:]]*:[[:space:]]*)([Bb][Ee][Aa][Rr][Ee][Rr]|[Bb][Aa][Ss][Ii][Cc])[[:space:]]+[^[:space:]\"']+/\\1\\2 [REDACTED]/g"
